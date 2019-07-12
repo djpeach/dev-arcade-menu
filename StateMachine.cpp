7e6a24c8e76aebb06b1f6970aa0c8ee1f7a83284
Daniel Peach <dpeaches96@gmail.com>
2019-07-11 22:29:08 -0400

    added statemachine

---
+++ StateMachine.cpp
@@ -0,0 +1,43 @@
+#include "StateMachine.hpp"
+
+namespace peachgames {
+  void StateMachine::addState(StateRef newState, bool isReplacing) {
+    isAdding = true;
+    isReplacing = isReplacing;
+    newState = std::move(newState);
+  }
+
+  void StateMachine::removeState() {
+    isRemoving = true;
+  }
+
+  void StateMachine::processStateChanges() {
+    if (isRemoving && !states.empty()) {
+      states.pop();
+
+      if (!states.empty()) {
+        states.top()->resume();
+      }
+
+      isRemoving = false;
+    }
+
+    if (isAdding) {
+      if (!states.empty()) {
+        if (isReplacing) {
+          states.pop();
+        } else {
+          states.top()->pause();
+        }
+      }
+
+      states.push(std::move(newState));
+      states.top()->init();
+      isAdding = false;
+    }
+  }
+
+  StateRef& StateMachine::getActiveState() {
+    return states.top();
+  }
+}
--- StateMachine.hpp
+++ StateMachine.hpp
@@ -1,1 +1,31 @@
 #pragma once
+
+#include <memory>
+#include <stack>
+
+#include "State.hpp"
+
+namespace peachgames {
+  typedef std::unique_ptr<State> StateRef;
+
+  class StateMachine {
+  public:
+    StateMachine() {};
+    ~StateMachine() {};
+
+    void addState(StateRef newState, bool isReplacing = true);
+    void removeState();
+
+    void processStateChanges();
+
+    StateRef& getActiveState();
+
+  private:
+    std::stack<StateRef> states;
+    StateRef newState;
+
+    bool isRemoving;
+    bool isAdding;
+    bool isReplacing;
+  };
+}
